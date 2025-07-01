da2480ae77c9f528c8f2683b8445129c73c10471
Matvii Mashtaliar <matvii.mashtalyar@gmail.com>
2025-07-01 13:30:09 +0300

    Implement nft_mint

--- ts/cluster1/nft_mint.ts
+++ ts/cluster1/nft_mint.ts
@@ -1,26 +1,43 @@
-import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
-import { createSignerFromKeypair, signerIdentity, generateSigner, percentAmount } from "@metaplex-foundation/umi"
-import { createNft, mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
+import { createUmi } from '@metaplex-foundation/umi-bundle-defaults';
+import {
+  createSignerFromKeypair,
+  signerIdentity,
+  generateSigner,
+  percentAmount,
+} from '@metaplex-foundation/umi';
+import {
+  createNft,
+  mplTokenMetadata,
+} from '@metaplex-foundation/mpl-token-metadata';
 
-import wallet from "../turbin3-wallet.json"
-import base58 from "bs58";
+import wallet from '../turbin3-wallet.json';
+import base58 from 'bs58';
 
-const RPC_ENDPOINT = "https://api.devnet.solana.com";
+const RPC_ENDPOINT = 'https://api.devnet.solana.com';
 const umi = createUmi(RPC_ENDPOINT);
 
 let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
 const myKeypairSigner = createSignerFromKeypair(umi, keypair);
 umi.use(signerIdentity(myKeypairSigner));
-umi.use(mplTokenMetadata())
+umi.use(mplTokenMetadata());
 
 const mint = generateSigner(umi);
 
-(async () => {
-    // let tx = ???
-    // let result = await tx.sendAndConfirm(umi);
-    // const signature = base58.encode(result.signature);
-    
-    // console.log(`Succesfully Minted! Check out your TX here:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`)
+const metadataUrl = 'https://gateway.irys.xyz/47DdoRtPMeth3uUfPZoHgjfAeDXm3Zj3WeFCq6GFkg7w';
 
-    console.log("Mint Address: ", mint.publicKey);
+(async () => {
+  let tx = createNft(umi, {
+    mint,
+    name: 'Rotor',
+    sellerFeeBasisPoints: percentAmount(10),
+    uri: metadataUrl,
+  });
+  let result = await tx.sendAndConfirm(umi);
+  const signature = base58.encode(result.signature);
+
+  console.log(
+    `Succesfully Minted! Check out your TX here:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`
+  );
+
+  console.log('Mint Address: ', mint.publicKey);
 })();
