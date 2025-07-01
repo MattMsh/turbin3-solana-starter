aacdeefe1602b1dd8eae7e2171ea25272ab23428
Matvii Mashtaliar <matvii.mashtalyar@gmail.com>
2025-07-01 12:05:35 +0300

    Implement spl_init

--- ts/cluster1/spl_init.ts
+++ ts/cluster1/spl_init.ts
@@ -1,19 +1,28 @@
-import { Keypair, Connection, Commitment } from "@solana/web3.js";
+import { Keypair, Connection, Commitment } from '@solana/web3.js';
 import { createMint } from '@solana/spl-token';
-import wallet from "../turbin3-wallet.json"
+import wallet from '../turbin3-wallet.json';
 
 // Import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
 
 //Create a Solana devnet connection
-const commitment: Commitment = "confirmed";
-const connection = new Connection("https://api.devnet.solana.com", commitment);
+const commitment: Commitment = 'confirmed';
+const connection = new Connection('https://api.devnet.solana.com', commitment);
+const DECIMALS = 6;
+const mintKeypair = Keypair.generate();
 
 (async () => {
-    try {
-        // Start here
-        // const mint = ???
-    } catch(error) {
-        console.log(`Oops, something went wrong: ${error}`)
-    }
-})()
+  try {
+    const mint = await createMint(
+      connection,
+      keypair,
+      keypair.publicKey,
+      keypair.publicKey,
+      DECIMALS,
+      mintKeypair
+    );
+    console.log(`Mint created: ${mint.toBase58()}`);
+  } catch (error) {
+    console.log(`Oops, something went wrong: ${error}`);
+  }
+})();
