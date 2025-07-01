40958e675512b98f2cb8280dd79caeae82c40b7f
Matvii Mashtaliar <matvii.mashtalyar@gmail.com>
2025-07-01 12:34:42 +0300

    Implement spl_metadata

--- ts/cluster1/spl_metadata.ts
+++ ts/cluster1/spl_metadata.ts
@@ -1,49 +1,80 @@
-import wallet from "../turbin3-wallet.json"
-import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
-import { 
-    createMetadataAccountV3, 
-    CreateMetadataAccountV3InstructionAccounts, 
-    CreateMetadataAccountV3InstructionArgs,
-    DataV2Args
-} from "@metaplex-foundation/mpl-token-metadata";
-import { createSignerFromKeypair, signerIdentity, publicKey } from "@metaplex-foundation/umi";
-import { bs58 } from "@coral-xyz/anchor/dist/cjs/utils/bytes";
+import wallet from '../turbin3-wallet.json';
+import { createUmi } from '@metaplex-foundation/umi-bundle-defaults';
+import {
+  createMetadataAccountV3,
+  CreateMetadataAccountV3InstructionAccounts,
+  CreateMetadataAccountV3InstructionArgs,
+  DataV2Args,
+  findMetadataPda,
+  MPL_TOKEN_METADATA_PROGRAM_ID,
+} from '@metaplex-foundation/mpl-token-metadata';
+import {
+  createSignerFromKeypair,
+  signerIdentity,
+  publicKey,
+} from '@metaplex-foundation/umi';
+import { bs58 } from '@coral-xyz/anchor/dist/cjs/utils/bytes';
+import { PublicKey } from '@solana/web3.js';
 
 // Define our Mint address
-const mint = publicKey("<mint address>")
+const mint = publicKey('CjW6hvCkdmURVPUoSpdLMPoXZ8qfGnBwxuR1PFqxhh9p');
 
 // Create a UMI connection
 const umi = createUmi('https://api.devnet.solana.com');
 const keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
 const signer = createSignerFromKeypair(umi, keypair);
-umi.use(signerIdentity(createSignerFromKeypair(umi, keypair)));
+umi.use(signerIdentity(signer));
 
 (async () => {
-    try {
-        // Start here
-        // let accounts: CreateMetadataAccountV3InstructionAccounts = {
-        //     ???
-        // }
+  try {
+    const metadata_seeds = [
+      Buffer.from('metadata'),
+      new PublicKey(MPL_TOKEN_METADATA_PROGRAM_ID).toBuffer(),
+      new PublicKey(mint).toBuffer(),
+    ];
+    const [metadata, _bump] = PublicKey.findProgramAddressSync(
+      metadata_seeds,
+      new PublicKey(MPL_TOKEN_METADATA_PROGRAM_ID)
+    );
+    // Alternative approach
+    // const metadataUmi = findMetadataPda(
+    //   { eddsa: umi.eddsa, programs: umi.programs },
+    //   { mint }
+    // );
+    // console.log({ metadata, metadataUmi }); // SAME ADDRESS
 
-        // let data: DataV2Args = {
-        //     ???
-        // }
+    const accounts: CreateMetadataAccountV3InstructionAccounts = {
+      mint,
+      mintAuthority: signer,
+      metadata: publicKey(metadata),
+      updateAuthority: signer,
+      payer: signer,
+    };
 
-        // let args: CreateMetadataAccountV3InstructionArgs = {
-        //     ???
-        // }
+    const data: DataV2Args = {
+      name: 'Turbin3',
+      symbol: 'TRB',
+      uri: 'https://irys.xyz/',
+      sellerFeeBasisPoints: 0,
+      collection: null,
+      creators: null,
+      uses: null,
+    };
 
-        // let tx = createMetadataAccountV3(
-        //     umi,
-        //     {
-        //         ...accounts,
-        //         ...args
-        //     }
-        // )
+    let args: CreateMetadataAccountV3InstructionArgs = {
+      data,
+      isMutable: true,
+      collectionDetails: null,
+    };
 
-        // let result = await tx.sendAndConfirm(umi);
-        // console.log(bs58.encode(result.signature));
-    } catch(e) {
-        console.error(`Oops, something went wrong: ${e}`)
-    }
+    let tx = createMetadataAccountV3(umi, {
+      ...accounts,
+      ...args,
+    });
+
+    let result = await tx.sendAndConfirm(umi);
+    console.log(bs58.encode(result.signature));
+  } catch (e) {
+    console.error(`Oops, something went wrong: ${e}`);
+  }
 })();
